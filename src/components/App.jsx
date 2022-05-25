9f8ec53c35a8fe9d12a5acb0959ee37a271e1399
scottjohnson <scottwwj@gmail.com>
2022-05-24 21:26:25 -0500

    Add temp resume file download

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import {Route, Routes} from "react-router-dom";
 
-import './App.styles.scss';
-
 import Navigation from "./navigation/Navigation.component";
 import Resume from "./resume/resume.component";
 import Projects from "./projects/projects.component";
-import Home from "./home/home.component";
+import Home from "./home/Home.component";
 
 const App = () =>
   <div className="App">
     <Routes>
       <Route path={'/'} element={<Navigation/>}>
         <Route index element={<Home/>}/>
-        <Route path={'/resume'} element={<Resume/>}/>
         <Route path={'/projects'} element={<Projects/>}/>
         <Route path={'/personal'} element={<Resume/>}/>
       </Route>
--- src/components/navigation/Navigation.component.jsx
+++ src/components/navigation/Navigation.component.jsx
@@ -1,6 +1,7 @@
 import {Link, Outlet} from "react-router-dom";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {solid} from '@fortawesome/fontawesome-svg-core/import.macro'
+import resume from '../../resources/testfile.txt'
 
 import './Navigation.syles.scss'
 
@@ -11,9 +12,11 @@ const Header = () =>
         <FontAwesomeIcon icon={solid('house')}/>
       </Link>
       <div className={'navigation'}>
-        <Link to={'/resume'}>Resume</Link>
         <Link to={'/projects'}>Projects</Link>
         <Link to={'/personal'}>Personal</Link>
+        <a href={resume} download={'scott_johnson_resume.txt'}>
+          Resume <FontAwesomeIcon icon={solid('cloud-arrow-down')}/>
+        </a>
       </div>
     </div>
     <Outlet/>
