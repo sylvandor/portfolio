c2bb8efc866192d51db5b2c3d1e5bd093d0865f3
scottjohnson <scottwwj@gmail.com>
2022-05-24 22:03:40 -0500

    Tweak some fonts, colors, and padding.

--- src/components/home/Home.component.jsx
+++ src/components/home/Home.component.jsx
@@ -4,15 +4,13 @@ import './Home.styles.scss';
 
 const Home = () =>
   <div className={'home'}>
-    <h1 className={'name'}>Scott Johnson</h1>
+    <h1 className={'name'}>Hi, I'm Scott</h1>
     <div className={'image-container'}>
       <img src={pic} alt={'profile pic'}/>
     </div>
-
-    <h1>
+    <h1 className={'welcome'}>
       Welcome,
     </h1>
-    <h2>As you can tell, my name's Scott.</h2>
     <h3>
       I'm a software engineer. I decided to make this page as a portfolio for my professional work and
       talk about myself a bit. Feel free to navigate with the links up top.
