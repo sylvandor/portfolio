a64db7412fef2140047cba0528a1f804eab95540
scottjohnson <scottwwj@gmail.com>
2022-05-30 07:46:18 -0500

    Remove pwa setup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import App from './components/App';
 
@@ -20,5 +19,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-serviceWorkerRegistration.register();
